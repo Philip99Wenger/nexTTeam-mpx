06e2345d29733031f2a6b9e4a562918fa6eddd83
tmh0023 <tmh0023@oem-VirtualBox>
2020-02-03 21:43:33 -0500

    polling version 1.0

--- modules/polling.c
+++ modules/polling.c
@@ -8,19 +8,63 @@
 
 int poll(char * buffer, int * count){
 	int position=0;
+	int totalChars=0;
 	int flag=1;
 	char letter;
+	int i;
 	//char temp[1]={0};
-	*count=0;
+	//buffer[0]='\r';
 	
 	while(flag){
 		if (inb(COM1+5)&1){
 			letter=inb(COM1);
-			buffer[position]=letter;
-			position=position+1;
+			switch (letter){
+			case '~':
+				return 1;
+				break;
+			case 127:
+				if (position>0){
+					position=position-1;
+					buffer[position]='\0';
+					totalChars=totalChars-1;
+
+				}
+				else if (position==0){
+					buffer[position]='\0';
+
+				}
+				break;
+			default:
+				if (position < *count ){
+
+					buffer[position]=letter;
+					position=position+1;
+					totalChars=totalChars+1;
+				}
+				
+			
+
+			}
+			if (totalChars>0){
+				for(i=0;(i<totalChars+5)&&(i<*count);i++){
+					serial_print(" ");
+				}
+				serial_print("   \r");
+
+				
+				serial_print(strcat(buffer,"\r"));
+			}
+			else if (totalChars==0){
+				for(i=0;i<totalChars+5;i++){
+					serial_print(" ");
+				}
+				serial_print("   \r");
+			}
+			
 			//temp[0]=letter;
 			//serial_print(temp);
-			serial_print(strcat("\r",buffer));
+			
+			//serial_print(buffer);
 		
 		}
 
