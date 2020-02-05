847f0962084ffd677f2bcfff160c41afa028e352
tmh0023 <tmh0023@oem-VirtualBox>
2020-02-05 18:57:19 -0500

    polling version 0.8, redone printing, added arrow keys, delete testing

--- modules/polling.c
+++ modules/polling.c
@@ -11,26 +11,68 @@ int poll(char * buffer, int * count){
 	int totalChars=0;
 	int flag=1;
 	char letter;
-	int i;
+	char sublet;
+	//i=42;
+	char tempLetAr[2]={'\0','\0'};
+	int doPrint;
 	//char temp[1]={0};
 	//buffer[0]='\r';
 	
 	while(flag){
 		if (inb(COM1+5)&1){
 			letter=inb(COM1);
+			tempLetAr[0]=letter;
+			doPrint=1;
 			switch (letter){
-			case '~':
-				return 1;
+			case 91:
+				sublet=inb(COM1);
+				
+				if (sublet==51){
+					doPrint=0;
+					//tempLetAr[0]='%';
+					//serial_print(tempLetAr);
+					if (position<totalChars){
+						
+						serial_print("\[1C");
+						tempLetAr[0]='\b';
+						serial_print(tempLetAr);
+						//tempLetAr[0]='%';
+						//serial_print(tempLetAr);	
+					}	
+					
+				}
+				else if(sublet==67&&position< *count){
+					doPrint=0;
+					if(position==totalChars){
+						buffer[position]=' ';
+						totalChars=totalChars+1;
+
+					}
+					
+					position=position+1;
+					serial_print("\[1C");
+				}
+				else if(sublet==68&&position>0){
+					doPrint=0;
+					position=position-1;
+					serial_print("\[1D");
+
+				}
+				/*else{
+					serial_print("HEYHELLO");
+				}*/
 				break;
 			case 127:
 				if (position>0){
 					position=position-1;
 					buffer[position]='\0';
 					totalChars=totalChars-1;
+					tempLetAr[0]='\b';
 
 				}
 				else if (position==0){
 					buffer[position]='\0';
+					tempLetAr[0]='\b';
 
 				}
 				break;
@@ -45,20 +87,35 @@ int poll(char * buffer, int * count){
 			
 
 			}
-			if (totalChars>0){
-				for(i=0;(i<totalChars+5)&&(i<*count);i++){
+			if (totalChars>0&&position < *count&&doPrint){
+				/*for(i=0;(i<totalChars+5)&&(i<*count);i++){
 					serial_print(" ");
 				}
 				serial_print("   \r");
 
 				
-				serial_print(strcat(buffer,"\r"));
+				serial_print(strcat(buffer,"\r"));*/
+				//i=i+1;
+				serial_print(tempLetAr);
+				if (tempLetAr[0]=='\b'){
+					tempLetAr[0]=' ';
+					serial_print(tempLetAr);
+					tempLetAr[0]='\b';
+					serial_print(tempLetAr);
+				}
 			}
-			else if (totalChars==0){
-				for(i=0;i<totalChars+5;i++){
+			else if (totalChars==0&&doPrint){
+				/*for(i=0;i<totalChars+5;i++){
 					serial_print(" ");
 				}
-				serial_print("   \r");
+				serial_print("   \r");*/
+				serial_print(tempLetAr);
+				if (tempLetAr[0]=='\b'){
+					tempLetAr[0]=' ';
+					serial_print(tempLetAr);
+					tempLetAr[0]='\b';
+					serial_print(tempLetAr);
+				}
 			}
 			
 			//temp[0]=letter;
