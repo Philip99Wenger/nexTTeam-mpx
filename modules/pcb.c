0daf683cf6a35aaa80a58a2c9f517371d8c4224e
Jessica Hammersla <jlh0065@mix.wvu.edu>
2020-02-17 17:47:18 -0500

    Finish insertPCB

--- modules/pcb.c
+++ modules/pcb.c
@@ -28,7 +28,85 @@ void insertPCB(pcb* Pcb){
 			previous = current;
 			current = current->pcbNext;
 		}
-		
+		// if pcb is the lowest priority
+		current = readyQueue.tail;
+		Pcb->pcbPrev = current;
+		Pcb->pcbNext = NULL;
+		current->pcbNext = Pcb;
+		readyQueue.tail = Pcb;
+		readyQueue.count++;
+		return;
+	}
+	else if(Pcb->stateRRB == READY && Pcb->stateIsSuspended == SUSPENDED){
+		pcb* current = suspendReadyQueue.head;
+		pcb* previous;
+		if(Pcb->priority > current->priority){
+				suspendReadyQueue.head = Pcb;
+				current->pcbPrev = Pcb;
+				Pcb->pcbNext = current;
+				suspendReadyQueue.count++;
+				return;
+			}
+		while(current){ // place priority where needed in queue
+			if(Pcb->priority > current->priority){
+				previous->pcbNext = current;
+				Pcb->pcbPrev = previous;
+				current->pcbPrev = Pcb;
+				Pcb->pcbNext = current;
+				suspendReadyQueue.count++;
+				return;
+			}
+			previous = current;
+			current = current->pcbNext;
+		}
+		// if pcb is the lowest priority
+		current = suspendReadyQueue.tail;
+		Pcb->pcbPrev = current;
+		Pcb->pcbNext = NULL;
+		current->pcbNext = Pcb;
+		suspendReadyQueue.tail = Pcb;
+		suspendReadyQueue.count++;
+		return;
+	}
+
+	else if(Pcb->stateRRB == BLOCKED && Pcb->stateIsSuspended == NOTSUSPENDED){
+		if(blockedQueue.count == 0){
+				Pcb->pcbPrev = NULL;
+				Pcb->pcbNext = NULL;
+				blockedQueue.head = Pcb;
+				blockedQueue.tail = Pcb;
+				blockedQueue.count++;
+				return;
+			}
+		else{
+				blockedQueue.tail->pcbNext = Pcb;
+				Pcb->pcbNext = NULL;
+				Pcb->pcbPrev = blockedQueue.tail;
+				blockedQueue.tail = Pcb;
+				blockedQueue.count++;
+				return;
+				
+			}
+	}
+
+	else if(Pcb->stateRRB == BLOCKED && Pcb->stateIsSuspended == SUSPENDED){
+		if(suspendedBlockedQueue.count == 0){
+				Pcb->pcbPrev = NULL;
+				Pcb->pcbNext = NULL;
+				suspendedBlockedQueue.head = Pcb;
+				suspendedBlockedQueue.tail = Pcb;
+				suspendedBlockedQueue.count++;
+				return;
+			}
+		else{
+				suspendedBlockedQueue.tail->pcbNext = Pcb;
+				Pcb->pcbNext = NULL;
+				Pcb->pcbPrev = blockedQueue.tail;
+				suspendedBlockedQueue.tail = Pcb;
+				suspendedBlockedQueue.count++;
+				return;
+				
+			}
 	}
 }
 
