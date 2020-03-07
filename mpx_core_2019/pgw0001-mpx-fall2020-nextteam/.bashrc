ac8c0f5e88280a2883f88523f5baf19afcff85a9
Jessica Hammersla <jlh0065@mix.wvu.edu>
2020-03-07 03:49:19 -0500

    Trying again

---
+++ mpx_core_2019/pgw0001-mpx-fall2020-nextteam/.bashrc
@@ -0,0 +1,1 @@
+source cs450-setup
---
+++ mpx_core_2019/pgw0001-mpx-fall2020-nextteam/modules/context.h
@@ -0,0 +1,26 @@
+#ifndef _CONTEXT_H
+#define _CONTEXT_H
+
+#include <stdint.h>
+#include <string.h>
+#include <system.h>
+
+#include <core/io.h>
+#include <core/serial.h>
+#include <core/tables.h>
+#include <core/interrupts.h>
+#include <mem/heap.h>
+#include <mem/paging.h>
+
+#include "mpx_supt.h"
+#include "pcb.h"
+
+typedef struct context{
+	u32int gs, fs, es, ds;
+	u32int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+	u32int eip, cs, eflags;
+}context;
+
+u32int* sys_call(context *registers);
+
+#endif
